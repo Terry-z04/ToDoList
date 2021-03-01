e718fe89936911ae168c9fa2757481a331c5a2ea
Terry-zzy <62125938+Terry-zzy@users.noreply.github.com>
2021-03-01 12:05:59 -0500

    Chapter 5.6 An Intro to Structs Completed

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Terry Zhuang on 3/1/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
