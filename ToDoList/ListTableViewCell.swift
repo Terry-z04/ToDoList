ef1d8ff48b6e24fc6e7ec102c8bcd8ae458ec578
Terry-zzy <62125938+Terry-zzy@users.noreply.github.com>
2021-03-07 18:22:44 -0500

    Chapter 5.11 Adding Checkmarks via Custom Table View Cells + Protocols Completed

---
+++ ToDoList/ListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ListTableViewCell.swift
+//  ToDoList
+//
+//  Created by Terry Zhuang on 3/7/21.
+//
+
+import UIKit
+
+protocol ListTableViewCellDelegate: class {
+    func checkBoxToggle(sender: ListTableViewCell)
+}
+
+class ListTableViewCell: UITableViewCell {
+
+    weak var delegate: ListTableViewCellDelegate?
+    
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBAction func checkToggled(_ sender: UIButton) {
+        delegate?.checkBoxToggle(sender: self)
+    }
+    
+
+}
--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -150,16 +150,25 @@ class ToDoListViewController: UIViewController {
     }
     
 }
-extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource, ListTableViewCellDelegate {
+    func checkBoxToggle(sender: ListTableViewCell) {
+        
+        if let selectedIndexPath = tableView.indexPath(for: sender) {
+            toDoItems[selectedIndexPath.row].completed = !toDoItems[selectedIndexPath.row].completed
+            tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
+            saveData()
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("😎 numberOfRowInSection was just called. Returning \(toDoItems.count)")
         return toDoItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("🚣🏿 cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoItems[indexPath.row])")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = toDoItems[indexPath.row].name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ListTableViewCell
+        cell.delegate = self
+        cell.nameLabel.text = toDoItems[indexPath.row].name
+        cell.checkBoxButton.isSelected = toDoItems[indexPath.row].completed
         return cell
     }
     
