85b07ec5f44483c7f4c808a5d384d65b4b0e377f
Terry-zzy <62125938+Terry-zzy@users.noreply.github.com>
2021-03-08 00:26:12 -0500

    Chapter 5.14 Refactoring our Code + an Intro to Model View Controller MVC + @escaping closures Completed

---
+++ ToDoList/ToDoItems.swift
@@ -0,0 +1,39 @@
+//
+//  ToDoItems.swift
+//  ToDoList
+//
+//  Created by Terry Zhuang on 3/7/21.
+//
+
+import Foundation
+
+class ToDoItems {
+    var itemsArray: [ToDoItem] = []
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todo").appendingPathExtension("json")
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(itemsArray)
+        do {
+            try data?.write(to: documentURL, options: .noFileProtection)
+        } catch {
+            print("😡 ERROR: Could not save data \(error.localizedDescription)")
+        }
+    }
+    
+    func loadData(completed: @escaping ()->() ) {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todo").appendingPathExtension("json")
+
+        guard let data = try? Data(contentsOf: documentURL) else {return}
+        let jsonDecoder = JSONDecoder()
+        do {
+            itemsArray = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
+            
+        } catch {
+            print("😡 ERROR: Could not load data \(error.localizedDescription)")
+        }
+        completed() // flag says we are done to run the closure
+    }
+}
